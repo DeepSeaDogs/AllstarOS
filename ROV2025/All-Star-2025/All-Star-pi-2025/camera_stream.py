f008a6967323f48d408389c6d708443681e7f1d8
Jacob <jacob44@mail.com>
2025-06-18 19:35:58 -0700

    Update camera_stream.py

--- ROV2025/All-Star-2025/All-Star-pi-2025/camera_stream.py
+++ ROV2025/All-Star-2025/All-Star-pi-2025/camera_stream.py
@@ -11,7 +11,7 @@ conn, addr = server_socket.accept()
 print(f"Connected to: {addr}")
 
 cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'YUYV'))
+cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 cap.set(cv2.CAP_PROP_FPS, 30)
